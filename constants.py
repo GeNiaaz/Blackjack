b52d07bf558d3d02c939056c25568777b5394d25
GeNiaaz <geniaaz1009@gmail.com>
2023-09-01 02:31:48 +0800

    update constants

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["Hearts", "Diamonds", "Spades", "Clubs"]
+ROYALTY_SUITS = ["J", "Q", "K"]
+ROYALTY_VALUE = 10
+NUMBER_OF_CARDS = len(RANKS) * len(SUITS)
+SHUFFLE_THRESHOLD = 15
 SUITS_SYMBOLS = {"Hearts": "♥", "Diamonds": "♦", "Spades": "♠", "Clubs": "♣"}
