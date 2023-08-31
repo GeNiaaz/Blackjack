bb77f21b69b3cf441d44b98c0c9ea66a241cba10
GeNiaaz <geniaaz1009@gmail.com>
2023-09-01 02:32:58 +0800

    abstract out logic for shuffling

--- deck.py
+++ deck.py
@@ -1,14 +1,19 @@
 import random
-from constants import RANKS, SUITS
+from constants import RANKS, SUITS, NUMBER_OF_CARDS, SHUFFLE_THRESHOLD
 from card import Card
 
 class Deck:
     def __init__(self):
         self.cards = [Card(rank, suit) for rank in RANKS for suit in SUITS]
-        random.shuffle(self.cards)
+        self.shuffle_cards()
     
+    def shuffle_cards(self):
+        if len(self.cards) < SHUFFLE_THRESHOLD or len(self.cards) == NUMBER_OF_CARDS:
+            random.shuffle(self.cards)
+
     def deal(self):
         if self.cards:
+            self.shuffle_cards()
             return self.cards.pop()
         else:
             return None
